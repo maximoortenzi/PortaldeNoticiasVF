583b33adb06ef7c581b7a9b48dcb6f276bb48c03
Alej035 <alejo@settimini.net>
2026-06-15 15:18:45 -0300

    Agrega función editarNoticia() que carga los datos en el formulario para modificar

---
+++ js/admin.js
@@ -0,0 +1,75 @@
+if(!sessionStorage.getItem("admin")){
+    window.location.href = "login.html";
+}
+
+mostrarNoticiasAdmin();
+
+document.getElementById("newsForm").addEventListener("submit", agregarNoticia);
+
+function agregarNoticia(e){
+
+    e.preventDefault();
+
+    const titulo = document.getElementById("titulo").value;
+    const descripcion = document.getElementById("descripcion").value;
+    const imagen = document.getElementById("imagen").value;
+
+    const noticias = obtenerNoticias();
+
+    noticias.push({ titulo, descripcion, imagen });
+
+    guardarNoticias(noticias);
+    mostrarNoticiasAdmin();
+    e.target.reset();
+
+}
+
+function mostrarNoticiasAdmin(){
+
+    const contenedor = document.getElementById("listaNoticias");
+    const noticias = obtenerNoticias();
+
+    contenedor.innerHTML = "";
+
+    noticias.forEach((noticia, index) => {
+
+        contenedor.innerHTML += `
+        <div class="card">
+            <img src="${noticia.imagen}">
+            <h3>${noticia.titulo}</h3>
+            <p>${noticia.descripcion}</p>
+            <button onclick="eliminarNoticia(${index})">Eliminar</button>
+            <button onclick="editarNoticia(${index})">Editar</button>
+        </div>
+        `;
+
+    });
+
+}
+
+function eliminarNoticia(index){
+
+    const noticias = obtenerNoticias();
+    noticias.splice(index, 1);
+    guardarNoticias(noticias);
+    mostrarNoticiasAdmin();
+
+}
+
+function editarNoticia(index){
+
+    const noticias = obtenerNoticias();
+    const noticia = noticias[index];
+
+    document.getElementById("titulo").value = noticia.titulo;
+    document.getElementById("descripcion").value = noticia.descripcion;
+    document.getElementById("imagen").value = noticia.imagen;
+
+    eliminarNoticia(index);
+
+}
+
+document.getElementById("logoutBtn").addEventListener("click", () => {
+    sessionStorage.removeItem("admin");
+    window.location.href = "index.html";
+});
